7bd08958e71ead4504bdcaac41afa662495a1485
Shreya-bansall <shreyab2400@gmail.com>
2023-04-19 23:37:32 +0530

    key

--- key.py
+++ key.py
@@ -2,15 +2,13 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import ec
 
 # Generate ECC key pair
-private_key = ec.generate_private_key(ec.SECP256R1())  # You can choose a different curve if needed
+private_key = ec.generate_private_key(ec.SECP256R1())
 public_key = private_key.public_key()
 
-# Serialize public key to PEM format
 public_key_pem = public_key.public_bytes(
     encoding=serialization.Encoding.PEM,
     format=serialization.PublicFormat.SubjectPublicKeyInfo
 )
 
-# Save public key to a file
 with open('public_key.pem', 'wb') as f:
     f.write(public_key_pem)
